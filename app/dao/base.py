04958d29af97a011bbac911ac9ef2708dfa5aa44
shara-code <shara666.r@gmail.com>
2025-04-18 23:06:43 +0300

    feat: Добавлен метод find_by_id для поиска записи по ID в классе BaseDAO

--- app/dao/base.py
+++ app/dao/base.py
@@ -7,6 +7,13 @@ class BaseDAO:
     model = None
 
     @classmethod
+    async def find_by_id(cls, id: int):
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(id=id)
+            result = await session.execute(query)
+            return result.mappings().one_or_none()
+
+    @classmethod
     async def find_one_or_none(cls, **filter_by):
         async with async_session_maker() as session:
             query = select(cls.model.__table__.columns).filter_by(**filter_by)
